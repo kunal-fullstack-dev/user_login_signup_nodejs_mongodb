3474a20d368296e1484931c1f65d763845a6e0ed
kunal-fullstack-dev <132496547+kunal-fullstack-dev@users.noreply.github.com>
2023-05-11 18:42:07 +0530

    Documentation added

--- app/controllers/cookie.controller.js
+++ app/controllers/cookie.controller.js
@@ -1,32 +1,30 @@
 const config = require("../config/auth.config");
 var jwt = require("jsonwebtoken");
-
 const cookie_data = require('../models/user_cookie.model');
 
+/*
+This API used to isnet LI cookie value, we need to pass Cookie value and also user login token in header 'x-access-token
+*/
 
 exports.setcookie =async (req,res) =>
 {
   try{
 	  
        if(req.body.cookie_value )
-       {
-		   
+       {		   
 		  const authHeader = req.headers["x-access-token"];
-		  let decoded = jwt.verify(authHeader, config.secret);
-			  
+		  let decoded = jwt.verify(authHeader, config.secret);			  
 			const set_cookie =  new cookie_data({
 			  user_id:decoded.id,
 			  cookie_value: req.body.cookie_value,
-			});
-          
-			  set_cookie.save((err) => {
+			});          
+			set_cookie.save((err) => {
 					if (err) {
-					 return res.status(500).send({ message: err });
-					   
+					 return res.status(500).send({ message: err });					   
 					}else{
 						return res.send({"msg":"Cookie value submitted succesfully"});
 					}
-			   });
+			});
        }
        else
        {
